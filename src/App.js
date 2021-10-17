b78fd807c5dcebf19307dc3b7c96fba6df8a1ddb
Deo55 <d.bogic@hotmail.com>
2021-10-17 21:49:19 +0200

    Front-end rijesen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
+import Home from './components/pages/Home/Home';
+import AddMovie from './components/pages/AddMovie/AddMovie';
+import EditMovie from './components/pages/EditMovie/EditMovie';
+import Navigation from "./components/Navigation/Navigation";
+
+import "./App.css";
+import './index.css';
+
+//status message incase there is an error on add/edit movie
+//search filter based on title/genre
+// placeholder radio button/dropdown menu??--
+//navigation not sticky anymore?
+//rating not working on decimal?
+//mouseover/leave on moviecards 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <BrowserRouter>
+        <div>
+          <Navigation />
+          <div className= 'wrapper'>
+            <Switch>
+              <Route exact path= '/' component={Home} />
+              <Route exact path= '/movie/add' component={AddMovie} />
+              <Route exact path= '/movie/edit/:id' component={EditMovie}/>
+            </Switch>
+        </div>
+      </div>
+      </BrowserRouter>
+    );
+  }
 
 export default App;
