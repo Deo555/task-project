490290d1da4fff8410b1f9dec59c32709a555f6b
Deo555 <83168196+Deo555@users.noreply.github.com>
2021-10-18 13:46:21 +0200

    Add files via upload

---
+++ components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <Link to="/"> HOME</Link>
+      <Link to="/movie/add">ADD MOVIE</Link>
+    </nav>
+  );
+};
+
+export default Navigation;
