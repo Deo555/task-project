589dda2d25e35604a6f5744837d5a64f334c587c
Deo555 <83168196+Deo555@users.noreply.github.com>
2021-10-18 22:29:00 +0200

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,17 +1,14 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './Navigation.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 const Navigation = () => {
-    return(
-        <nav>
-            <Link to="/"> HOME</Link>
-            <Link to="/movie/add">ADD MOVIE</Link>
-        </nav>
+  return (
+    <nav>
+      <Link to="/"> HOME</Link>
+      <Link to="/movie/add">ADD MOVIE</Link>
+    </nav>
+  );
+};
 
-    )
-  }
-  
-  export default Navigation;
-
-  
+export default Navigation;
